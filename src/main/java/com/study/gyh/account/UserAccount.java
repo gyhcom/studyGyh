20c2adcea3b91f108af84d8c9898078a66977fc5
gyhcom <gyeonho@gmail.com>
2024-01-16 22:42:03 +0900

    FEAT:Account Authentication Create

---
+++ src/main/java/com/study/gyh/account/UserAccount.java
@@ -0,0 +1,18 @@
+package com.study.gyh.account;
+
+import com.study.gyh.domain.Account;
+import java.util.List;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+@Getter
+public class UserAccount extends User {
+    private Account account;
+
+    public UserAccount(Account account) {
+        super(account.getNickname(), account.getPassword(),
+            List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        this.account = account;
+    }
+}
---
+++ src/main/java/com/study/gyh/main/MainController.java
@@ -0,0 +1,20 @@
+package com.study.gyh.main;
+
+import com.study.gyh.account.CurrentUser;
+import com.study.gyh.domain.Account;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(@CurrentUser Account account, Model model) {
+        if (account != null) {
+            model.addAttribute(account);
+        }
+
+        return "index";
+    }
+}
