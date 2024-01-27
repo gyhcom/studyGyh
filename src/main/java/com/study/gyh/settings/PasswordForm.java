59e9f37d24210603292ffb4d6144904b9969edbe
gyhcom <gyeonho@gmail.com>
2024-01-27 23:24:17 +0900

    FEAT: Password Update

---
+++ src/main/java/com/study/gyh/settings/PasswordForm.java
@@ -0,0 +1,12 @@
+package com.study.gyh.settings;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class PasswordForm {
+    @Length(min = 8, max = 50)
+    private String newPassword;
+    @Length(min = 8, max = 50)
+    private String newPasswordConfirm;
+}
--- src/main/java/com/study/gyh/settings/SettingsController.java
+++ src/main/java/com/study/gyh/settings/SettingsController.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,26 +18,58 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class SettingsController {
 
-    static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
-    static final String SETTINGS_PROFILE_URL = "/settings/profile";
-    private final AccountService accountService;
-    @GetMapping("/settings/profile")
-    public String profileUpdateForm(@CurrentUser Account account, Model model) {
-        model.addAttribute(account);
-        model.addAttribute(new Profile(account));
-        return "settings/profile";
+    @InitBinder("passwordForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(new PasswordFormValidator());
+
     }
 
-    @PostMapping("/settings/profile")
+    static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
+    static final String SETTINGS_PROFILE_URL = "/settings/profile";
+
+    static final String SETTINGS_PASSWORD_VIEW_NAME = "settings/password";
+    static final String SETTINGS_PASSWORD_URL = "/settings/password";
+
+    private final AccountService accountService;
+
+    @GetMapping(SETTINGS_PROFILE_URL)
+    public String updateProfileForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new Profile(account));
+        return SETTINGS_PROFILE_VIEW_NAME;
+    }
+
+    @PostMapping(SETTINGS_PROFILE_URL)
     public String updateProfile(@CurrentUser Account account,
         @Valid Profile profile, Errors errors, Model model, RedirectAttributes attributes) {
         if (errors.hasErrors()) {
             model.addAttribute(account);
-            return "settings/profile";
+            return SETTINGS_PROFILE_VIEW_NAME;
         }
 
         accountService.updateProfile(account, profile);
         attributes.addFlashAttribute("message", "프로필 수정완료");
         return "redirect:" + SETTINGS_PROFILE_URL;
+    }
+
+    @GetMapping(SETTINGS_PASSWORD_URL)
+    public String updatePasswordForm(@CurrentUser Account account, Model model) {
+        model.addAttribute(account);
+        model.addAttribute(new PasswordForm());
+
+        return SETTINGS_PASSWORD_VIEW_NAME;
+    }
+
+    @PostMapping(SETTINGS_PASSWORD_URL)
+    public String updatePassword(@CurrentUser Account account, @Valid PasswordForm passwordForm,
+        Errors errors, Model model, RedirectAttributes attributes) {
+        if (errors.hasErrors()) {
+            model.addAttribute(account);
+            return SETTINGS_PASSWORD_VIEW_NAME;
+        }
+
+        accountService.updatePassword(account, passwordForm.getNewPassword());
+        attributes.addFlashAttribute("message", "패스워드를 변경했습니다.");
+        return "redirect:" + SETTINGS_PASSWORD_URL;
     }
 }
